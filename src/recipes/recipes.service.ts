f1e756d3a153a3e560e77367754bc467c81bf7b6
PAPINO90 <papinimo2015@gmail.com>
2025-03-03 13:40:39 +0000

    Update recipes.service.ts

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Recipe } from './recipe.interface.';
+
+import { v4 as uuidv4} from 'uuid';
 
 @Injectable()
-export class RecipesService {}
+export class RecipesService {
+    private recipes: Recipe[] = []; 
+
+    getRecipes(): Recipe[] {
+        return this.recipes;
+
+    }
+
+    getRecipe(id: string): Recipe {
+        return this.recipes.find((recipe) => recipe.id === id);
+
+
+    }
+
+
+    createRecipe(recipe: Recipe): Recipe{
+        const newRecipe = { id: uuidv4(), ...recipe };
+        this.recipes.push(newRecipe);
+        return newRecipe;
+    }
+}
