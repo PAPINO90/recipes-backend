1acc08f22f6156bdfff829fb0c374b921ae5f25f
PAPINO90 <papinimo2015@gmail.com>
2025-03-03 13:39:46 +0000

    Update recipes.controller.ts

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { RecipesService } from './recipes.service';
+import { Recipe } from './recipe.interface';
+
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 @Controller('recipes')
-export class RecipesController {}
+export class RecipesController {
+constructor(private readonly recipesService: RecipesService)
+{}
+@Get()
+getRecipes(){
+    return this.recipesService.getRecipes();
+}
+
+
+
+@Get(':id')
+getRecipe(id: string) {
+    return this.recipesService.createRecipe(id);
+
+}
+
+
+
+@Post()
+createRecipe(@Body recipe: Recipe){
+    return this.recipesService.createRecipe(recipe);
+
+}
+
+}
